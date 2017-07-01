4a4d3163cd2f9926c8fad5b72eae749abadeb00e
pexeer <pexeer@gmail.com>
2017-07-01 23:43:16 +0800

    update time test.

--- t.cpp
+++ t.cpp
@@ -1,27 +1,93 @@
 #include "p/base/time.h"
 #include <stdio.h>
+#include <iostream>
+#include <chrono>
 
 
-unsigned long long f() {
-    return p::base::clock_gettime_us();
+inline unsigned long long f() {
+    return p::base::clock_gettime_ns();
 }
 
-unsigned long long g() {
+inline unsigned long long g() {
     return p::base::gettimeofday_us();
 }
 
+inline unsigned long long h() {
+    return p::base::steady_clock_gettime_ns();
+}
 
-int chenzongjia_afjk() {
-    int i = 0;
-    for (long long j = 0; j < 1000000000; ++j) {
+inline unsigned long long h11() {
+    auto now = std::chrono::steady_clock::now();
+    return now.time_since_epoch().count();
+}
+
+inline unsigned long long bench_f() {
+    unsigned long long i = 0;
+    for (long long j = 0; j < 10000000; ++j) {
         i += f();
     }
     return i;
 }
 
+inline unsigned long long bench_g() {
+    unsigned long long i = 0;
+    for (long long j = 0; j < 10000000; ++j) {
+        i += g();
+    }
+    return i;
+}
+
+inline unsigned long long bench_h() {
+    unsigned long long i = 0;
+    for (long long j = 0; j < 10000000; ++j) {
+        i += h();
+    }
+    return i;
+}
+
+inline unsigned long long bench_h11() {
+    unsigned long long i = 0;
+    for (long long j = 0; j < 10000000; ++j) {
+        i += h11();
+    }
+    return i;
+}
+
 int main() {
-    printf("start_abc_defgllu\n");
-    printf("%d\n", chenzongjia_afjk());
-    printf("end_fuck_you\n");
+    auto f0 = f();
+    auto g0 = g();
+    auto h0 = h();
+    auto h110 = h11();
+    std::cout << f0 << std::endl;
+    std::cout << g0 << std::endl;
+    std::cout << h0 << std::endl;
+    std::cout << h110 << std::endl;
+
+    {
+        auto start = h();
+        auto ret = bench_f();
+        auto end = h();
+        std::cout <<  end - start << "\t" << ret << std::endl;
+    }
+    {
+        auto start = h();
+        auto ret = bench_g();
+        auto end = h();
+        std::cout <<  end - start << "\t" << ret << std::endl;
+    }
+    {
+        auto start = h();
+        auto ret = bench_h();
+        auto end = h();
+        std::cout <<  end - start << "\t" << ret << std::endl;
+    }
+
+    {
+        auto start = h();
+        auto ret = bench_h11();
+        auto end = h();
+        std::cout <<  end - start << "\t" << ret << std::endl;
+    }
+
     return 0;
 }
